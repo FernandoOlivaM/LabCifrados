170aaab40db53f31624beca3a4663968bca23261
Fernando Oliva <feroliv4z@gmail.com>
2019-10-04 11:17:50 -0600

    Se completo el cifrado horizontal y vertical en espiral, aun falta decifrar el texto contenido en el archivo

--- Lab-3_1251518_1229918/Models/CifradoEspiral.cs
+++ Lab-3_1251518_1229918/Models/CifradoEspiral.cs
@@ -10,25 +10,16 @@ namespace Lab_3_1251518_1229918.Models
     {
         string RutaUsuario = string.Empty;
         string texto = string.Empty;
+        string textoMatriz = string.Empty;
 
         public void CifrarMensaje(string rutaAchivos, string archivoLeido, int m, bool direccion)
         {
             RutaUsuario = rutaAchivos;
             LeerArchivo(archivoLeido);
             GenerarMatrizCifrado(m, direccion);
-
+            EscribirEnArchivoCifrado(textoMatriz);
         }
-        public void escribirArchivo(string texto)
-        {
-            using (var writeStream = new FileStream(RutaUsuario + "\\..\\Files\\archivoCifradoEspiral.cif", FileMode.OpenOrCreate))
-            {
-                using (var writer = new BinaryWriter(writeStream))
-                {
-                    writer.Seek(0, SeekOrigin.End);
-                    writer.Write(System.Text.Encoding.Unicode.GetBytes(texto));
-                }
-            }
-        }
+     
         public void LeerArchivo(string archivoLeido)
         {
             int bufferLength = 1024;
@@ -66,7 +57,7 @@ namespace Lab_3_1251518_1229918.Models
                 //llenando matriz horizontalmente
                 for (int p = 0; p < valorM; p++)
                 {
-                    for (int j = valorN - 1; j > -1; j--)
+                    for (int j =0; j < valorN; j++)
                     {
                         if (contadorTexto != texto.Length)
                         {
@@ -79,6 +70,8 @@ namespace Lab_3_1251518_1229918.Models
                         }
                     }
                 }
+
+                recorrerHaciaAbajo(valorM, valorN, matriz);
             }
             else
             {
@@ -98,10 +91,49 @@ namespace Lab_3_1251518_1229918.Models
                         }
                     }
                 }
+                recorrerHaciaDerecha(valorM, valorN, matriz);
             }
+            
+           
+        }
+        public void recorrerHaciaAbajo(int valorM, int valorN , string[,] matriz)
+        {
             //recorriendo matriz en esprial
-            //al introducir el texto horizontalmente se hizo de derecha a izquierda
-            string textoMatriz = string.Empty;
+            int i, auxiliarM = 0, auxiliarN = 0;
+            while (auxiliarM < valorM && auxiliarN < valorN)
+            {
+                for (i = auxiliarM; i < valorM; ++i)
+                {
+                    textoMatriz += matriz[i, auxiliarN];
+                }
+                auxiliarN++;
+                for (i = auxiliarN; i < valorN; ++i)
+                {
+                    textoMatriz += matriz[valorM - 1, i];
+                }
+                valorM--;
+                if (auxiliarN < valorN)
+                {
+                    for (i = valorM - 1; i >= auxiliarM; --i)
+                    {
+                        textoMatriz += matriz[i, valorN - 1];
+                    }
+                    valorN--;
+                }
+                if (auxiliarM < valorM)
+                {
+                    for (i = valorN - 1; i >= auxiliarN; --i)
+                    {
+                        textoMatriz += matriz[auxiliarM, i];
+                    }
+                    auxiliarM++;
+                }
+            }
+            EscribirEnArchivoCifrado(textoMatriz);
+        }
+        public void recorrerHaciaDerecha(int valorM, int valorN, string[,] matriz)
+        {
+            //recorriendo matriz en esprial
             int i, auxiliarM = 0, aulixiarN = 0;
             while (auxiliarM < valorM && aulixiarN < valorN)
             {
@@ -114,7 +146,7 @@ namespace Lab_3_1251518_1229918.Models
                 {
                     textoMatriz += matriz[i, valorN - 1];
                 }
-                valorN--; 
+                valorN--;
                 if (auxiliarM < valorM)
                 {
                     for (i = valorN - 1; i >= aulixiarN; --i)
@@ -132,22 +164,94 @@ namespace Lab_3_1251518_1229918.Models
                     aulixiarN++;
                 }
             }
+        }
+        public void EscribirEnArchivoCifrado(string texto)
+        {
             //se escribe el texto cifrado en el archivo
             using (var writeStream = new FileStream(RutaUsuario + "\\..\\Files\\archivoCifradoEspiral.cif", FileMode.OpenOrCreate))
             {
                 using (var writer = new BinaryWriter(writeStream))
                 {
                     writer.Seek(0, SeekOrigin.End);
-                    writer.Write(System.Text.Encoding.Unicode.GetBytes(textoMatriz));
+                    writer.Write(System.Text.Encoding.Unicode.GetBytes(texto));
+                }
+            }
+            textoMatriz = string.Empty;
+        }
+        public void EscribirEnArchivoDecifrado(string texto)
+        {
+            //se escribe el texto cifrado en el archivo
+            using (var writeStream = new FileStream(RutaUsuario + "\\..\\Files\\archivoDecifradoEspiral.txt", FileMode.OpenOrCreate))
+            {
+                using (var writer = new BinaryWriter(writeStream))
+                {
+                    writer.Seek(0, SeekOrigin.End);
+                    writer.Write(System.Text.Encoding.Unicode.GetBytes(texto));
                 }
             }
         }
-        
         public void DecifrarMensaje(string rutaAchivos, string archivoLeido, int m, bool direccion)
         {
             RutaUsuario = rutaAchivos;
             LeerArchivo(archivoLeido);
-           // GenerarMatrizDecifrado(m, direccion);
+            GenerarMatrizDecifrado(m, direccion);
+        }
+        public void GenerarMatrizDecifrado(int valorM, bool direccion)
+        {
+            int contadorCaracteresReales = 0;
+            foreach(var letra in texto)
+            {
+                if (Convert.ToString(letra) != "\0" || Convert.ToString(letra) != "$")
+                {
+                    contadorCaracteresReales++;
+                }
+            }
+            var valorN = this.texto.Length / valorM;
+            int contadorTexto = 0;
+            if (this.texto.Length % valorM != 0)
+            {
+                valorN++;
+            }
+            string[,] matriz = new string[valorM, valorN];
+            if (direccion)
+            {
+                int i, auxiliarM = 0, aulixiarN = 0;
+                while (auxiliarM < valorM && aulixiarN < valorN)
+                {
+                    for (i = aulixiarN; i < valorN; ++i)
+                    {
+                        matriz[auxiliarM, i] = Convert.ToString(texto[contadorTexto]);
+                        contadorTexto++;
+                    }
+                    auxiliarM++;
+                    for (i = auxiliarM; i < valorM; ++i)
+                    {
+                         matriz[i, valorN - 1] = Convert.ToString(texto[contadorTexto]);
+                        contadorTexto++;
+                    }
+                    valorN--;
+                    if (auxiliarM < valorM)
+                    {
+                        for (i = valorN - 1; i >= aulixiarN; --i)
+                        {
+                            matriz[valorM - 1, i] = Convert.ToString(texto[contadorTexto]);
+                            contadorTexto++;
+                        }
+                        valorM--;
+                    }
+                    if (aulixiarN < valorN)
+                    {
+                        for (i = valorM - 1; i >= auxiliarM; --i)
+                        {
+                            matriz[i, aulixiarN] = Convert.ToString(texto[contadorTexto]);
+                            contadorTexto++;
+                        }
+                        aulixiarN++;
+                    }
+                }
+
+            }
+
         }
     }
 }
