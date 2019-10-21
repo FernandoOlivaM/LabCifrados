24a1b2752ae7f41963515318d0ccd2c035824f79
feroliv4z <feroliv4z@gmail.com>
2019-10-20 18:08:33 -0600

    generando valores para llave publica, aun falta mejorar condiciones del while

--- Lab-3_1251518_1229918/Models/CifradoRSA.cs
+++ Lab-3_1251518_1229918/Models/CifradoRSA.cs
@@ -7,9 +7,34 @@ namespace Lab_3_1251518_1229918.Models
 {
     public class CifradoRSA
     {
-       public void GenerarLlaves(int p, int q)
+        public void GenerarLlaves(int p, int q)
         {
+            //lave publica
+            var n = p * q;
+            var phi = (p - 1) * (q - 1);
+            var e = 2;
+            while (e < phi)
+            {
+                if (MaximoComunDivisor(e, phi) == 1)
+                    break;
+                else
+                    e++;
+            }
+        }
+        //funcion para maximo comun divisor
+        private int MaximoComunDivisor(int n1, int n2)
+        {
+            while (n1 != 0 && n2 != 0)
+            {
+                if (n1 > n2)
+                {
+                    n1 %= n2;
+                }
+                else
+                    n2 %= n1;
+            }
 
+            return n1 == 0 ? n2 : n1;
         }
     }
 }
